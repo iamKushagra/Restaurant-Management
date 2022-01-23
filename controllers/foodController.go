e821474c7ac343217fe5942f121219d461d09fc3
Kushagra Kesav <kushagra198@gmail.com>
2022-01-24 00:06:55 +0530

    Food/Menu Controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -34,8 +34,28 @@ func GetFoods() gin.HandlerFunc {
 		startIndex: = (page - 1) * recordPerPage
 		startIndex, err = strvconv.Atoi(c.Query("start_index"))
 
-		matchStage := bson.D{{ "$match", bson.D{{"category", c.Query("category")}}}}
-		groupStage := bson.D{{ "$group", bson.D{{"_id",bson.D{{"_id","null"}},{"total_count",bson.D{{"$sum",1}}},}}}}
+		matchStage := bson.D{
+			{ "$match", bson.D{
+				{"category", c.Query("category")},
+			}}}
+
+		groupStage := bson.D{
+			{ "$group", bson.D{
+				{"_id",bson.D{{"_id","null"}}},
+				{"total_count",bson.D{{"$sum",1}}},
+				{"data",bson.D{{"$push","$$ROOT"}}},
+			}}}
+
+		projectStage := bson.D{
+			{ "$project", bson.D{
+				{"_id",0},
+				{"total_count",1},
+				{"food_items",bson.D{{"$slice",[]interface{}{"$data",startIndex,recordPerPage}}}},
+			}}}
+
+		foodCollection.Aggregate(ctx,mongo.Pipeline{
+			matchStage, groupStage, projectStage
+		})
 	}
 }
 
